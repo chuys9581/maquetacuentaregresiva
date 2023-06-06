3a87703d6ed0f81da33e06dbeed6709d93003acf
Jesus Jimenez <chuy.dev.f@gmail.com>
2023-06-06 16:52:07 -0600

    ultimos detalles

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,24 +30,23 @@ setInterval(actualizarContador, 1000);
 
 
 // Función para reproducir el video seleccionado
-function reproducirVideo(url) {
-    var reproductor = document.getElementById("reproductor");
+const reproducirVideo = (url) => {
+    let reproductor = document.getElementById("reproductor");
     reproductor.innerHTML = '<iframe width="100%" height="100%" src="' + url + '" frameborder="0" allowfullscreen></iframe>';
 }
 
 // Precargar y reproducir el primer video al cargar la página
 document.addEventListener("DOMContentLoaded", function () {
-    var primerVideo = document.querySelector(".video-thumbnail:first-child");
-    var urlPrimerVideo = primerVideo.dataset.url;
+    let primerVideo = document.querySelector(".video-thumbnail:first-child");
+    let urlPrimerVideo = primerVideo.dataset.url;
     reproducirVideo(urlPrimerVideo);
 });
 
-// Asociar eventos de clic a los videos
 document.addEventListener("DOMContentLoaded", function () {
-    var videos = document.getElementsByClassName("video-thumbnail");
-    for (var i = 0; i < videos.length; i++) {
+    let videos = document.getElementsByClassName("video-thumbnail");
+    for (let i = 0; i < videos.length; i++) {
         videos[i].addEventListener("click", function () {
-            var url = this.dataset.url;
+            let url = this.dataset.url;
             reproducirVideo(url);
         });
     }
