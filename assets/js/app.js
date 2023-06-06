f975060a46ea74b828d480d8ef67368caf72dc54
Jesus Jimenez <chuy.dev.f@gmail.com>
2023-06-06 16:23:47 -0600

    maqueta cuenta regresiba terminada

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+const actualizarContador = () => {
+
+    const fechaObjetivo = new Date("August 30, 2023 00:00:00").getTime();
+    const fechaActual = new Date().getTime();
+
+    let diferencia = fechaObjetivo - fechaActual;
+
+    // Cálculos para obtener días, horas, minutos y segundos restantes
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    // Mostrar el contador en los elementos correspondientes
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+    // Si la fecha objetivo ha pasado, mostrar un mensaje de finalización
+    if (diferencia < 0) {
+      document.getElementById("contador").innerHTML = "El contador ha finalizado";
+    }
+}
+
+setInterval(actualizarContador, 1000);
+
+
+
+
+
+// Función para reproducir el video seleccionado
+function reproducirVideo(url) {
+    var reproductor = document.getElementById("reproductor");
+    reproductor.innerHTML = '<iframe width="100%" height="100%" src="' + url + '" frameborder="0" allowfullscreen></iframe>';
+}
+
+// Precargar y reproducir el primer video al cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+    var primerVideo = document.querySelector(".video-thumbnail:first-child");
+    var urlPrimerVideo = primerVideo.dataset.url;
+    reproducirVideo(urlPrimerVideo);
+});
+
+// Asociar eventos de clic a los videos
+document.addEventListener("DOMContentLoaded", function () {
+    var videos = document.getElementsByClassName("video-thumbnail");
+    for (var i = 0; i < videos.length; i++) {
+        videos[i].addEventListener("click", function () {
+            var url = this.dataset.url;
+            reproducirVideo(url);
+        });
+    }
+});
